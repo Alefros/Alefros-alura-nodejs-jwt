51ad8118b06f3da7350ea87b8e8ee499540cb761
Alefros <alef.r.oliveira@gmail.com>
2021-07-09 00:16:36 -0300

    Alteração dos atributos do e-mail de verificação

--- src/usuarios/emails.js
+++ src/usuarios/emails.js
@@ -1,30 +1,50 @@
 const nodemailer = require('nodemailer');
-const { modelo } = require('.');
 
-class Email {
+const configuracaoEmailProducao = {
+  host: process.env.EMAIL_HOST,
+  auth: {
+    user: process.env.EMAIL_USUARIO,
+    pass: process.env.EMAIL_SENHA
+  },
+  secure: true
+};
 
-    async enviaEmail() {
-        const contaTeste = await nodemailer.createTestAccount();
-        const transportador = nodemailer.createTransport({
-            host: 'smto.ethereal.email',
-            auth: contaTeste,
-        });
-        const info = await transportador.sendMail(this);
-    
-        console.log('URL:' + nodemailer.getTestMessageUrl(info));
-    }
+const configuracaoEmailTeste = (contaTeste) => ({
+  host: 'smtp.ethereal.email',
+  auth: contaTeste,
+});
+
+
+async function criaConfiguracaoEmail() {
+  if (process.env.NODE_ENV === 'production') {
+    return configuracaoEmailProducao;
+  } else {
+    const contaTeste = await nodemailer.createTestAccount();
+    return configuracaoEmailTeste(contaTeste);
+  }
 }
 
-class EmailVerificacao extends Email{
-    constructor(usuario, endereco) {
-        super();
-        this.from = '"Blog do Código" <noreply@blogdocodigo.com.br>';
-        this.to = usuario.email;
-        this.subject = '"Verificação de email'
-        this.text = `Verfique seu email aqui: ${endereco}`
-        this.html = `Verfique seu email aqui: ${endereco}`
-        
+class Email {
+  async enviaEmail() {
+    const configuracaoEmail = await criaConfiguracaoEmail();
+    const transportador = nodemailer.createTransport(configuracaoEmail);
+    const info = await transportador.sendMail(this);
+  
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('URL: ' + nodemailer.getTestMessageUrl(info));
     }
+  }
+}
+
+class EmailVerificacao extends Email {
+  constructor(usuario, endereco) {
+    super();
+    this.from = '"Blog do Código" <noreply@blogdocodigo.com.br>';
+    this.to = usuario.email;
+    this.subject = 'Verificação de e-mail';
+    this.text = `Olá! Verifique seu e-mail aqui: ${endereco}`;
+    this.html = `<h1>Olá!</h1> Verifique seu e-mail aqui: <a href="${endereco}">${endereco}</a>`;
+  }
 }
 
 module.exports = { EmailVerificacao };
